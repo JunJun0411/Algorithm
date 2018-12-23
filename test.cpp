ea2dd8a950d58aefd95fbf315c46e0f6efb979a0
JunJun0411 <wnsgudrnrn@naver.com>
2018-12-23 17:42:52 +0900

    third Commit

--- test.cpp
+++ test.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main(){
-    int n; cin>>n;
-    
-    cout << "JUN JJANG   "<<'\n';z
+    string input;  
+    cin >> input;
+    cout<< input;
+    cout << "JUN JJANG   "<<'\n';
 } 
