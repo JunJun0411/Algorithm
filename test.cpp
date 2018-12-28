dd31573b5a8ec60836f176a5a68a1a43bcb49473
JunJun0411 <wnsgudrnrn@naver.com>
2018-12-28 19:29:05 +0900

    7

--- test.cpp
+++ test.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
-int Max(int a, int b){ return a> b ? a:b;}
-int Dp[301] = {};
-int Ap[301] = {};
+int z=0, o=0;
+int fibonacci(int n) {
+    if (n == 0) {
+        z++;
+        return 0;
+    } else if (n== 1) {
+        o++;
+        return 1;
+    } else {
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
 int main(){
-    int n; cin>>n;
-    for(int i=1;i<=n;i++){
-        cin >> Ap[i];
+    int T,N; cin>>T;
+    for(int i=0; i<T; i++){
+        printf("%d",)
+        fibonacci(N);
+        cout<<z <<" "<<o<<'\n';
+        z=0;
+        o=0;
     }
-    for(int i=1; i<=3&&i<=n;i++){
-        if(i!=3) Dp[i] = Ap[i] + Ap[i-1];   
-        else Dp[i] = Max(Ap[i]+Ap[i-1], Ap[i]+Ap[i-2]);
-    }
-    for(int i=4;i<=n;i++){
-        Dp[i]= Max(Ap[i] + Ap[i-1] + Dp[i-3], Ap[i] + Dp[i-2] );
-    }
-    cout<<Dp[n]<<'\n';
 }
