277bfe242821b97ed0b3579d8e6cb750030726f1
JunJun0411 <wnsgudrnrn@naver.com>
2018-12-28 16:58:03 +0900

    asdf

--- test.cpp
+++ test.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
-
+int Dp[1001][3]={};
 int main(){
-    string input;  
-    cin >> input;
-    cout<< input;
-    cout << "JUN JJANG   "<<'\n';
+    int n; cin>>n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<3; j++){
+            cin>> Dp[i][j];
+        }
+    }
+    
 } 
